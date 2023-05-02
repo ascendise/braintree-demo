95130d24aa1647e2701158a19c7385430b985eef
Ascendise <rapperido@gmail.com>
2023-05-02 23:33:25 +0200

    generate client token

--- backend/app.ts
+++ backend/app.ts
@@ -1,14 +1,24 @@
-import http from 'http';
 import express from 'express';
+import braintree from 'braintree'; 
+import dotenv from 'dotenv';
 
-const app = express()
-const port = 3000
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.MERCHANT_ID!,
+  publicKey: process.env.PUBLIC_KEY!,
+  privateKey: process.env.PRIVATE_KEY!
+});
+const app = express();
+const port = 3000;
+
+app.get('/client_token', async (req, res) => {
+  const response = await gateway.clientToken.generate({})
+  res.send(response.clientToken);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
 
